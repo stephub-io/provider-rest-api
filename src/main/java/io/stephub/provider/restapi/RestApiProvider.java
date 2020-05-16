3d9b2f78d56f5600148e114bdb29a2663fca1809
mbok <mboook@gmail.com>
2020-05-16 23:58:08 +0200

    Simplify step methods

--- src/main/java/io/stephub/provider/restapi/RestApiProvider.java
+++ src/main/java/io/stephub/provider/restapi/RestApiProvider.java
@@ -2,11 +2,13 @@ package io.stephub.provider.restapi;
 
 import io.stephub.provider.api.ProviderException;
 import io.stephub.provider.api.model.ProviderOptions;
-import io.stephub.provider.api.model.StepResponse;
 import io.stephub.provider.api.model.spec.PatternType;
-import io.stephub.provider.util.spring.SpringBeanProvider;
+import io.stephub.provider.util.StepFailedException;
+import io.stephub.provider.util.controller.LocalSpringBeanProviderAdapter;
 import io.stephub.provider.util.spring.annotation.StepArgument;
 import io.stephub.provider.util.spring.annotation.StepMethod;
+import lombok.Builder;
+import lombok.Getter;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -15,20 +17,18 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
-import static io.stephub.provider.api.model.StepResponse.StepStatus.FAILED;
-import static io.stephub.provider.api.model.StepResponse.StepStatus.PASSED;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 
 @Service
-public class RestApiProvider extends SpringBeanProvider<RestApiSessionState, RestApiOptions, Class<?>, Object> {
+public class RestApiProvider extends LocalSpringBeanProviderAdapter<RestApiSessionState, RestApiOptions> {
     @Override
     protected String getName() {
         return "rest-api";
     }
 
     @Override
-    protected Class<? extends RestApiOptions> getOptionsSchema() {
+    protected Class<? extends RestApiOptions> getOptionsSchemaClass() {
         return RestApiOptions.class;
     }
 
@@ -43,26 +43,30 @@ public class RestApiProvider extends SpringBeanProvider<RestApiSessionState, Res
     }
 
     @StepMethod(pattern = "a GET request {requestName} to {url}", patternType = PatternType.SIMPLE)
-    public StepResponse<Object> aRequest(final RestApiSessionState state,
-                                         @StepArgument(name = "requestName") final String requestName,
-                                         @StepArgument(name = "url") final String url) {
+    public void aRequest(final RestApiSessionState state,
+                         @StepArgument(name = "requestName") final String requestName,
+                         @StepArgument(name = "url") final String url) {
         state.getRequests().put(requestName, new Request.Builder().get().url(this.getUrl(state, url)).build());
-        return StepResponse.builder().status(PASSED).build();
+    }
+
+    @Builder
+    @Getter
+    public static class ApiResponse {
+        private final int status;
     }
 
     @StepMethod(pattern = "I execute request {requestName} getting response under {responseName}", patternType = PatternType.SIMPLE)
-    public StepResponse<Object> callRequest(final RestApiSessionState state,
-                                            @StepArgument(name = "requestName") final String requestName,
-                                            @StepArgument(name = "responseName") final String responseName) {
+    public ApiResponse callRequest(final RestApiSessionState state,
+                                   @StepArgument(name = "requestName") final String requestName,
+                                   @StepArgument(name = "responseName") final String responseName) {
         final Request httpRequest = this.getSafeRequest(state, requestName);
         try {
             final Response httpResponse = this.buildClient(state).newCall(httpRequest).execute();
-            return StepResponse.builder().status(PASSED).
-                    output(responseName, "TODO").
-                    build();
+            return ApiResponse.builder().
+                    status(httpResponse.code())
+                    .build();
         } catch (final IOException e) {
-            return StepResponse.builder().status(FAILED).
-                    build();
+            throw new StepFailedException(e);
         }
 
     }
